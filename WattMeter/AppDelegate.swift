6a01f85703f8c9fb5b7ba9ede704ebf139cea4ec
Lander Noterman <lander.noterman@ugent.be>
2018-04-24 19:54:09 +0200

    Initial Commit

---
+++ WattMeter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WattMeter
+//
+//  Created by Lander Noterman on 24/04/2018.
+//  Copyright © 2018 Lander Noterman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WattMeter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WattMeter
+//
+//  Created by Lander Noterman on 24/04/2018.
+//  Copyright © 2018 Lander Noterman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
